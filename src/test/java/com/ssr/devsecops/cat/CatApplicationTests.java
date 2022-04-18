9d25dadb535212f0ac5ace2d2530de7e4e265f60
ljwang <ljwang@thoughtworks.com>
2022-04-18 15:53:26 +0800

    Init

---
+++ src/test/java/com/ssr/devsecops/cat/CatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssr.devsecops.cat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
